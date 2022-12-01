10f838019b6fdef0ce831e58d5751f13ac1e8859
danmyd <118432290+danmyd@users.noreply.github.com>
2022-12-01 09:50:02 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Duty = 0
+basic.forever(function () {
+    while (Duty < 1023) {
+        pins.analogWritePin(AnalogPin.P0, Duty)
+        Duty = Duty + 1
+        basic.pause(10)
+    }
+    while (Duty > 0) {
+        pins.analogWritePin(AnalogPin.P0, Duty)
+        Duty = Duty - 1
+    }
+    basic.pause(10)
+})
